1e8bbd1251ba68af3ff887aa3a509b27f342ea6f
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-06 15:31:21 +0530

    List

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-//var React=require('react')
 import React from "react";
 import ReactDOM from "react-dom";
-//'var ReactDOM=require('react-dom');
-//html jaisa dikhne wala jsx
-//ReactDOM.render(kya,kahan,callback)
-//Bable inside the scene
-/*ReactDOM.render(
-  React.createElement("h1", null, "Hi There"),
-  document.getElementById("root")
-);*/
-/*var h1 = document.createElement("h1");
-h1.innerHTML = "Manish Kumar";
-document.getElementById("root").appendChild(h1);*/
-//ReactDOM.render(<><h1>Hi Manish</h1><h1>Hi rajat</h1></>,document.getElementById('root'));
-//ReactDOM.render(<div><h1>Hi Manish</h1><h1>Hi rajat</h1></div>,document.getElementById('root'));
-//ReactDOM.render([<h1>Manish</h1>,<h1>Rajat</h1>],document.getElementById('root'));
-
-//var arr = [<h1>manish</h1>, <h2>rajat</h2>, <h3>astu</h3>];
-//ReactDOM.render(arr, document.getElementById("root"));
 ReactDOM.render(
-  <React.Fragment>
-    <h1>Manish</h1>,<h1>Rajat</h1>
-  </React.Fragment>,
+  <>
+    <h1>List here</h1>
+    <p>Movie List</p>
+    <ol>
+      <li>No where</li>
+      <li>No guys</li>
+      <li>No girls</li>
+      <li>No out</li>
+      <li>No where</li>
+      <li>No where</li>
+    </ol>
+  </>,
   document.getElementById("root")
 );
