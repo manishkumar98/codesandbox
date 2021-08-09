99582ee67830853755ef485cf076ce869512f61c
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-09 09:56:20 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-//var React=require('react')
 import React from "react";
 import ReactDOM from "react-dom";
-//'var ReactDOM=require('react-dom');
-//html jaisa dikhne wala jsx
-//ReactDOM.render(kya,kahan,callback)
-//Bable inside the scene
-/*ReactDOM.render(
-  React.createElement("h1", null, "Hi There"),
-  document.getElementById("root")
-);*/
-/*var h1 = document.createElement("h1");
-h1.innerHTML = "Manish Kumar";
-document.getElementById("root").appendChild(h1);*/
-//ReactDOM.render(<><h1>Hi Manish</h1><h1>Hi rajat</h1></>,document.getElementById('root'));
-//ReactDOM.render(<div><h1>Hi Manish</h1><h1>Hi rajat</h1></div>,document.getElementById('root'));
-//ReactDOM.render([<h1>Manish</h1>,<h1>Rajat</h1>],document.getElementById('root'));
 
-var arr = [<h1>manish</h1>, <h2>rajat</h2>, <h3>astu</h3>];
-ReactDOM.render(arr, document.getElementById("root"));
+const name = "Manish";
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
+
+ReactDOM.render(
+  <>
+    <h>My name is {name}.</h>
+    <p>Date is {date}</p>
+    <p>Time is {time}</p>
+  </>,
+  document.getElementById("root")
+);
