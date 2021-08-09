bbc114a3e4546c0f653f1d5b34832b378e7183f1
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-09 15:22:24 +0530

    DateandTime

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-//var React=require('react')
+
 import React from "react";
 import ReactDOM from "react-dom";
-//'var ReactDOM=require('react-dom');
-//html jaisa dikhne wala jsx
-//ReactDOM.render(kya,kahan,callback)
-//Bable inside the scene
-/*ReactDOM.render(
-  React.createElement("h1", null, "Hi There"),
-  document.getElementById("root")
-);*/
-/*var h1 = document.createElement("h1");
-h1.innerHTML = "Manish Kumar";
-document.getElementById("root").appendChild(h1);*/
-//ReactDOM.render(<><h1>Hi Manish</h1><h1>Hi rajat</h1></>,document.getElementById('root'));
-//ReactDOM.render(<div><h1>Hi Manish</h1><h1>Hi rajat</h1></div>,document.getElementById('root'));
-//ReactDOM.render([<h1>Manish</h1>,<h1>Rajat</h1>],document.getElementById('root'));
 
-var arr = [<h1>manish</h1>, <h2>rajat</h2>, <h3>astu</h3>];
-ReactDOM.render(arr, document.getElementById("root"));
+let currDate=new Date(2020,5,5,14);
+currDate=currDate.getHours();
+let greeting="";
+const cssStyle={};
+if(currDate>=1&&currDate<12){
+  greeting="Morning";
+  cssStyle.color='green';
+}
+else if(currDate>=12&&currDate<18){
+  greeting="After";
+  cssStyle.color='orange';
+}
+if(currDate>=18&&currDate<20){
+  greeting="Evening";
+  cssStyle.color='red';
+}
+if(currDate>=20&&currDate<24){
+  greeting="night";
+  cssStyle.color='yellow';
+}
+ReactDOM.render(
+  <div>
+    <h1>Hello Sir,<span style={cssStyle}>{greeting}</span></h1>
+  </div>
+  , document.getElementById("root"));
