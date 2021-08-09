3a74f0362a254ef875bc3e3d716ca755a5361d88
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-09 10:37:35 +0530

    Anchor&Image

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 const name = "Manish";
-const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
-
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/200/300";
+const img3 = "https://picsum.photos/200/300";
+const img4 = "https://picsum.photos/200/300";
+const link = "https://www.google.com";
 ReactDOM.render(
   <>
-    <h>My name is {name}.</h>
-    <p>Date is {date}</p>
-    <p>Time is {time}</p>
+    <h1 contentEditable="true">My name is {name}</h1>
+    <img src={img1} alt="Random image" />
+    <img src={img2} alt="Random image" />
+    <img src={img3} alt="Random image" />
+    <a href={link} target="_blank">
+      <img src={img4} alt="Random image" />
+    </a>
   </>,
   document.getElementById("root")
 );
