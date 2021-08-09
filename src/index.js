4bad02e79116d17a495d091f732d66e755c82db5
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-09 11:38:07 +0530

    Internal Inlinecss

--- src/index.js
+++ src/index.js
@@ -7,9 +7,31 @@ const img2 = "https://picsum.photos/200/300";
 const img3 = "https://picsum.photos/200/300";
 const img4 = "https://picsum.photos/200/300";
 const link = "https://www.google.com";
+const heading = {
+  color: "#fa9191",
+  textAlign: "center",
+  textTransform: "capitalize",
+  fontWeight: "bold",
+  textShadow: "0px 2px 4px #ffe9cf",
+  margin: "50px",
+  fontFamily: "'Josefin Sans', sans-serif"
+};
 ReactDOM.render(
   <>
-    <h1 className="heading">My name is {name}</h1>
+    <h1 style={heading}>My name is {name}</h1>
+    <h1
+      style={{
+        color: "#fa9191",
+        textAlign: "center",
+        textTransform: "capitalize",
+        fontWeight: "bold",
+        textShadow: "0px 2px 4px #ffe9cf",
+        margin: "50px",
+        fontFamily: "'Josefin Sans', sans-serif"
+      }}
+    >
+      My name is {name}
+    </h1>
     <div className="img_div">
       <img src={img1} alt="Random image" />
       <img src={img2} alt="Random image" />
