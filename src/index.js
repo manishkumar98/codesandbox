f4b7d649c2c501a1f4f58d027d3226737658a5cb
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-09 11:17:23 +0530

    Margin and top

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 const name = "Manish";
 const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/200/300";
@@ -8,13 +9,15 @@ const img4 = "https://picsum.photos/200/300";
 const link = "https://www.google.com";
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">My name is {name}</h1>
-    <img src={img1} alt="Random image" />
-    <img src={img2} alt="Random image" />
-    <img src={img3} alt="Random image" />
-    <a href={link} target="_blank">
-      <img src={img4} alt="Random image" />
-    </a>
+    <h1 className="heading">My name is {name}</h1>
+    <div className="img_div">
+      <img src={img1} alt="Random image" />
+      <img src={img2} alt="Random image" />
+      <img src={img3} alt="Random image" />
+      <a href={link} target="_blank">
+        <img src={img4} alt="Random image" />
+      </a>
+    </div>
   </>,
   document.getElementById("root")
 );
